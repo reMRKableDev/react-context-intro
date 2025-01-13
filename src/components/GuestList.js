35c60ffc8308cfc75712b3ba80aa959d0b4057ee
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-01-13 18:36:59 +0100

    feat(GuestList.js): add tailwind styling

--- src/components/GuestList.js
+++ src/components/GuestList.js
@@ -3,10 +3,19 @@ import Guest from "./Guest";
 import { GuestContext } from "../contexts/GuestContext";
 
 export default class GuestList extends Component {
-  static contextType = GuestContext; // reading the context
+  static contextType = GuestContext;
 
   render() {
-    const { guestList } = this.context; // extracting context values
-    return guestList.map((guest, index) => <Guest key={index} guest={guest} />);
+    const { guestList } = this.context;
+
+    return (
+      <div className="flex flex-col gap-4 mt-4">
+        {guestList.map((guest, index) => (
+          <div key={index} className="animate-fadeIn">
+            <Guest guest={guest} />
+          </div>
+        ))}
+      </div>
+    );
   }
 }
