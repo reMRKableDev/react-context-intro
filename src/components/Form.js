86f41eba0ec4bfdb5eced220f7bfce5926503527
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-01-13 14:20:25 +0100

    feat: configure tailwindcss

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,32 +5,39 @@ export default class Form extends Component {
   render() {
     return (
       // reading the context
-      <GuestContext.Consumer>{(guestContext)=> {
-        // extracting context values
-        const {name, email, handleInputChange, handleSubmitForm} = guestContext
+      <GuestContext.Consumer>
+        {(guestContext) => {
+          // extracting context values
+          const { name, email, handleInputChange, handleSubmitForm } =
+            guestContext;
 
-        return(
-          <form onSubmit={handleSubmitForm}>
-            <label htmlFor="name">Name</label> <br />
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={handleInputChange}
-            />
-            <br />
-            <label htmlFor="email">Email</label> <br />
-            <input
-              type="text"
-              name="email"
-              value={email}
-              onChange={handleInputChange}
-            />
-            <br />
-            <button type="submit">Add Guest</button>
-        </form>
-        )
-      }}
+          return (
+            <form onSubmit={handleSubmitForm}>
+              <label htmlFor="name">Name</label> <br />
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={handleInputChange}
+              />
+              <br />
+              <label htmlFor="email">Email</label> <br />
+              <input
+                type="text"
+                name="email"
+                value={email}
+                onChange={handleInputChange}
+              />
+              <br />
+              <button
+                className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+                type="submit"
+              >
+                Add Guest
+              </button>
+            </form>
+          );
+        }}
       </GuestContext.Consumer>
     );
   }
