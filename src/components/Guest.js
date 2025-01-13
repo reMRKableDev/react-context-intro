d9f5dd1040e70e7b69f476501aea54f29dec28e8
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-01-13 15:59:04 +0100

    feat(Guest.js): add tailwind styling

--- src/components/Guest.js
+++ src/components/Guest.js
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 export default class Guest extends Component {
   render() {
     return (
-      <article>
-        <p>{this.props.guest.name}</p>
-        <p>{this.props.guest.email}</p>
+      <article className="bg-white shadow-md rounded-lg p-4 flex flex-col items-center text-gray-800 transition-transform transform hover:scale-105 duration-300">
+        <p className="text-lg font-semibold">{this.props.guest.name}</p>
+        <p className="text-gray-600">{this.props.guest.email}</p>
       </article>
     );
   }
